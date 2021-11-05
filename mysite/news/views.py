68b9e6896006747c45f9e60bd4658cfc56e7ad95
AlexGordeev <alexgordeev.vrn@gmail.com>
2021-11-05 21:53:13 +0300

    news template (lesson 1-11)

--- mysite/news/views.py
+++ mysite/news/views.py
@@ -1,10 +1,13 @@
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
+from .models import News
+
 
 def index(request: HttpRequest) -> HttpResponse:
-    return HttpResponse('Hello, world!')
-
-
-def test(request: HttpRequest) -> HttpResponse:
-    return HttpResponse('<h1>Тестовая страница</h1>')
+    news = News.objects.order_by('-created_at')
+    context = {
+        'news': news,
+        'title': 'Новости',
+    }
+    return render(request, 'news/index.html', context)
