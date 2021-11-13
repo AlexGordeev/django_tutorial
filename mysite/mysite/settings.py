6a0799d69b35178cd90f6081ae65a70d951cbf5a
AlexGordeev <alexgordeev.vrn@gmail.com>
2021-11-13 23:32:58 +0300

    static files (lesson 1-22)

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -120,9 +120,18 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+STATIC_ROOT = Path(BASE_DIR).joinpath('static')
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    Path(BASE_DIR).joinpath('mysite/static'),
+]
+
+
+# Media files
+
 MEDIA_ROOT = Path(BASE_DIR).joinpath('media')
 MEDIA_URL = '/media/'
-STATIC_URL = '/static/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
