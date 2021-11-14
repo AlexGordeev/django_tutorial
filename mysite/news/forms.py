57e9cc075201aa0cd6f9b762dc4aa95736c3622f
AlexGordeev <alexgordeev.vrn@gmail.com>
2021-11-14 12:16:23 +0300

    submit form (lesson 1-25)

--- mysite/news/forms.py
+++ mysite/news/forms.py
@@ -1,26 +1,14 @@
 from django import forms
 
-from .models import Category
+from .models import News
 
 
-class NewsForm(forms.Form):
-    title = forms.CharField(
-        label='Название',
-        max_length=150,
-        widget=forms.TextInput(attrs={'class': 'form-control'}),
-    )
-    content = forms.CharField(
-        label='Содержимое',
-        required=False,
-        widget=forms.Textarea(attrs={'class': 'form-control', 'rows': 5}),
-    )
-    is_published = forms.BooleanField(
-        initial=True,
-        label='Опубликовано',
-    )
-    category = forms.ModelChoiceField(
-        empty_label='Выберите категорию',
-        label='Категория',
-        queryset=Category.objects.all(),
-        widget=forms.Select(attrs={'class': 'form-control'}),
-    )
+class NewsForm(forms.ModelForm):
+    class Meta:
+        model = News
+        fields = ['title', 'content', 'is_published', 'category']
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control'}),
+            'content': forms.Textarea(attrs={'class': 'form-control', 'rows': 5}),
+            'category': forms.Select(attrs={'class': 'form-control'}),
+        }
--- mysite/news/views.py
+++ mysite/news/views.py
@@ -36,13 +36,10 @@ def add_news(request: HttpRequest) -> HttpResponse:
     if request.method == 'POST':
         form = NewsForm(request.POST)
         if form.is_valid():
-            news = News.objects.create(**form.cleaned_data)
+            news = form.save()
             return redirect(news)
     else:
         form = NewsForm()
 
-    context = {
-        'form': form
-    }
-
+    context = {'form': form}
     return render(request, 'news/add_news.html', context)
