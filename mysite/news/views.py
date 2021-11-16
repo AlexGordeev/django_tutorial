12412f8957093d80c7beda78e767256ff7e8f8ab
AlexGordeev <alexgordeev.vrn@gmail.com>
2021-11-16 22:26:02 +0300

    NewsByCategory class (lesson 1-28)

--- mysite/news/views.py
+++ mysite/news/views.py
@@ -14,20 +14,26 @@ class HomeNews(ListView):
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
         context['title'] = 'Главная страница'
+        context['has_link'] = True
         return context
 
     def get_queryset(self):
         return News.objects.filter(is_published=True)
 
 
-def get_category(request: HttpRequest, category_id: int) -> HttpResponse:
-    news = News.objects.filter(category_id=category_id)
-    category = get_object_or_404(Category, pk=category_id)
-    context = {
-        'news': news,
-        'category': category,
-    }
-    return render(request, 'news/category.html', context)
+class NewsByCategory(ListView):
+    model = News
+    template_name = 'news/home_news_list.html'
+    context_object_name = 'news'
+    allow_empty = False
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = Category.objects.get(pk=self.kwargs.get('category_id'))
+        return context
+
+    def get_queryset(self):
+        return News.objects.filter(category_id=self.kwargs.get('category_id'), is_published=True)
 
 
 def get_news_item(request: HttpRequest, news_id: int) -> HttpResponse:
