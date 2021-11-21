68985197ef73f44b0da84cdf8775ebe3cb286b48
AlexGordeev <alexgordeev.vrn@gmail.com>
2021-11-21 18:07:03 +0300

    views field (lesson 1-36)

--- mysite/news/models.py
+++ mysite/news/models.py
@@ -10,6 +10,7 @@ class News(models.Model):
     photo = models.ImageField(blank=True, upload_to='photos/%Y/%m/%d/', verbose_name='Фото')
     is_published = models.BooleanField(default=True, verbose_name='Опубликовано')
     category = models.ForeignKey('Category', on_delete=models.PROTECT, verbose_name='Категория')
+    views = models.IntegerField(default=0)
 
     def get_absolute_url(self) -> str:
         return reverse('news_item', kwargs={'pk': self.pk})
